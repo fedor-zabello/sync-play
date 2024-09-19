ee694c2c894a39627cfdf7797c6ca7f398c1492e
fedor-zabello <fedor.zabello@gmail.com>
2024-09-19 20:15:50 +0400

    modulith docs generation test

--- src/test/kotlin/org/pigletsinc/syncplay/modules/ApplicationModulesTest.kt
+++ src/test/kotlin/org/pigletsinc/syncplay/modules/ApplicationModulesTest.kt
@@ -2,12 +2,22 @@ package org.pigletsinc.syncplay.modules
 
 import org.pigletsinc.syncplay.SyncPlayApplication
 import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 import kotlin.test.Test
+
 
 class ApplicationModulesTest {
     @Test
     fun verifiesModularStructure() {
         val modules = ApplicationModules.of(SyncPlayApplication::class.java)
         modules.verify()
+    }
+
+    @Test
+    fun createModuleDocumentation() {
+        val modules: ApplicationModules = ApplicationModules.of(SyncPlayApplication::class.java)
+        Documenter(modules)
+            .writeDocumentation()
+            .writeIndividualModulesAsPlantUml()
     }
 }
