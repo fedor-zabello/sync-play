e1e595d485e3a5b4660485e3a0c2712aeea6de64
Fedor Zabello <153311587+fedor-zabello@users.noreply.github.com>
2024-09-29 17:50:01 +0400

    added oauth2 with google (#1)
    * oauth combined with current auth
    - refactoring pom.xml

---
+++ src/main/kotlin/org/pigletsinc/syncplay/shared/controller/LoginController.kt
@@ -0,0 +1,10 @@
+package org.pigletsinc.syncplay.shared.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class LoginController {
+    @GetMapping("/login")
+    fun getLoginPage() = "login"
+}
