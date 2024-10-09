e3b03b47662fbf64997574d56a2ccd344f627d16
fedor-zabello <fedor.zabello@gmail.com>
2024-10-09 21:44:35 +0400

    - flyway migration for users table - user entity - repository - upgraded spring version

--- src/main/kotlin/org/pigletsinc/syncplay/user/repository/UserRepository.kt
+++ src/main/kotlin/org/pigletsinc/syncplay/user/repository/UserRepository.kt
@@ -1,8 +1,8 @@
 package org.pigletsinc.syncplay.user.repository
 
 import org.pigletsinc.syncplay.user.entity.UserEntity
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository: CrudRepository<UserEntity, Long> {
+interface UserRepository: JpaRepository<UserEntity, Long> {
     fun findByEmail(email: String): UserEntity?
 }
--- src/main/resources/db/migration/V2__create_table_users.sql
+++ src/main/resources/db/migration/V2__create_table_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id       SERIAL PRIMARY KEY,
+    id       BIGINT PRIMARY KEY,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
