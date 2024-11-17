f629eeb561e3e696d1c79213cee360ac8fa1fab8
Svetlana Dotsenko <6469968lp@gmail.com>
2024-11-17 15:53:46 +0300

    spotless apply

--- src/main/kotlin/org/pigletsinc/syncplay/channel/entity/UserChannelMembership.kt
+++ src/main/kotlin/org/pigletsinc/syncplay/channel/entity/UserChannelMembership.kt
@@ -16,11 +16,9 @@ data class UserChannelMembership(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-
     @Column(name = "user_profile_id", nullable = false)
     val userProfileId: Long,
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "channel_id", nullable = false)
-    val channel: Channel
+    val channel: Channel,
 )
