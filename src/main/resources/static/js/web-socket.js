43054bdde13d0f2ee2cfe87b97b7b27d0a837181
ganin <ted.jondon@gmail.com>
2025-03-22 19:58:51 +0800

    fix sync lags

--- src/main/resources/static/js/web-socket.js
+++ src/main/resources/static/js/web-socket.js
@@ -3,11 +3,8 @@ import {loadVideoById, synchronizeVideo} from "./youtube-player.js";
 let stompClient = null;
 let socket = null;
 
-// Generate a unique client ID
-const clientId = Math.random().toString(36).substring(2, 15);
-
-// Keep track of message processing
 let processingMessage = false;
+const clientId = Math.random().toString(36).substring(2, 15);
 
 let currentChannelId = null;
 
@@ -44,7 +41,6 @@ export function connect(channelId) {
     });
 }
 
-// Send a sync message with the current video state
 export function sendSyncMessage(action, currentTime) {
     if (stompClient && stompClient.connected && !processingMessage) {
         stompClient.send("/app/videoSync/" + currentChannelId, {}, JSON.stringify({
@@ -52,11 +48,9 @@ export function sendSyncMessage(action, currentTime) {
             'time': currentTime,
             'clientId': clientId
         }));
-        console.log(`Sent ${action} message at time ${currentTime}`);
     }
 }
 
-// Send a syncSourceUrlOutput message with the video url
 export function sendSourceUrlSyncMessage(videoId) {
     if (stompClient && stompClient.connected) {
         stompClient.send("/app/syncSource/" + currentChannelId, {}, JSON.stringify({
@@ -66,29 +60,20 @@ export function sendSourceUrlSyncMessage(videoId) {
     }
 }
 
-// Handle sync messages received from the server
 function handleSyncMessage(message) {
     // Always ignore messages from ourselves
     if (message.clientId === clientId) {
-        console.log("Ignoring own message");
         return;
     }
-
-    console.log(`Received ${message.action} message from ${message.clientId} at time ${message.time}`);
-
-    // Set the processing flag to prevent sending a response
     processingMessage = true;
 
-    // Make sure the player properly responds to the message
     synchronizeVideo(message);
 
-    // Reset the processing flag after a short delay
     setTimeout(() => {
         processingMessage = false;
     }, 2000); // 2-second delay before allowing new messages to be sent
 }
 
-// Handle syncSourceUrl messages received from the server
 function handleSourceUrlMessage(syncSourceUrlMessage) {
     // Ignore messages from the same client
     if (syncSourceUrlMessage.clientId === clientId) {
